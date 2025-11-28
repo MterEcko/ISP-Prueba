eec0b74f28ec45816d0639c5a52dadb0e0a4739e
MterEcko <mt2finality@gmail.com>
2025-11-27 19:29:31 -0600

    update

--- backend/src/routes/chat.routes.js
+++ backend/src/routes/chat.routes.js
@@ -3,25 +3,6 @@ const router = express.Router();
 const chatController = require('../controllers/chat.controller');
 const authJwt = require('../middleware/auth.jwt');
 
-<<<<<<< Updated upstream
-// === CONVERSACIONES ===
-router.get('/conversations', [authJwt.verifyToken], chatController.getConversations);
-router.post('/conversations', [authJwt.verifyToken], chatController.createConversation);
-router.get('/conversations/:id/messages', [authJwt.verifyToken], chatController.getMessages);
-router.put('/conversations/:id/read', [authJwt.verifyToken], chatController.markAsRead);
-router.put('/conversations/:id', [authJwt.verifyToken], chatController.updateConversation);
-router.delete('/conversations/:id', [authJwt.verifyToken], chatController.deleteConversation);
-
-// === MENSAJES ===
-router.post('/messages', [authJwt.verifyToken], chatController.sendMessage);
-router.put('/messages/:id', [authJwt.verifyToken], chatController.updateMessage);
-router.delete('/messages/:id', [authJwt.verifyToken], chatController.deleteMessage);
-
-// === TELEGRAM ===
-router.get('/telegram/status', [authJwt.verifyToken], chatController.getTelegramStatus);
-
-module.exports = router;
-=======
 module.exports = function(app) {
   // === CONVERSACIONES ===
   app.get(
@@ -87,4 +68,3 @@ module.exports = function(app) {
   );
   
 };
->>>>>>> Stashed changes
